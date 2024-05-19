b8ba5221bbf0018432e769164d695b623bee9a3b
Notforest <notforest69@gmail.com>
2024-05-19 19:36:48 +0200

    Moved the initialization part to this module for cleaner main driver file.

--- src/gpio.c
+++ src/gpio.c
@@ -14,7 +14,7 @@
 #include"rpi.h"
 
 /* sets the new GPIO while parsing values and returning obtained errors */
-int set_gpio(union fan_config *config, uint8_t gpio) {
+int set_gpio(union fan_config *config, uint8_t old_gpio) {
     uint8_t new_gpio = config->gpio_num;
     // Parsing the configuration value.
     switch (new_gpio) {
@@ -34,21 +34,49 @@ int set_gpio(union fan_config *config, uint8_t gpio) {
     }
 
     // Changing the current GPIO state
-    if(gpio != new_gpio) { 
+    if(old_gpio != new_gpio) { 
         if(gpio_request(new_gpio, GPIO_NAME) < 0) {
             pr_err("%s: ERROR: GPIO_%d request failed.\n", THIS_MODULE->name, new_gpio);
-            return -EFAULT;
+            return -EACCES;
         }
+
+        gpio_set_value_cansleep(old_gpio, LOW);
+        gpio_free(old_gpio);
 
         gpio_direction_output(new_gpio, OUT);
         gpio_set_value(new_gpio, HIGH);
-
-        gpio_set_value_cansleep(gpio, LOW);
-        gpio_free(gpio);
     }
 
     pr_info("%s: New configuration is provided: GPIO_%d, PWM_MODE_%d\n", 
             THIS_MODULE->name, new_gpio, config->pwm_mode);
+
+    return 0;
+}
+
+/* Initializes the first available GPIO in the system. */
+int init_gpio(union fan_config *config) {
+    uint8_t gpio = GPIO_MIN;
+    for(; gpio < GPIO_AMOUNT; ++gpio) {
+        if(gpio_is_valid(gpio)) { 
+            // When a valid GPIO is found, requesting it.
+            if(gpio_request(gpio, GPIO_NAME) < 0) {
+                pr_err("%s: ERROR: GPIO_%d request failed...\n", THIS_MODULE->name, gpio);
+                gpio_free(gpio);
+            } else break;
+        }
+    }
+
+    // This check is true if all GPIO requests would fail or all GPIOs are not valid.
+    if(gpio == GPIO_AMOUNT) {
+        pr_err("%s: Requests failed. All pins are in use, aborting..\n", THIS_MODULE->name);
+        return -EACCES;
+    }   
+
+    gpio_direction_output(gpio, OUT);
+    gpio_set_value(gpio, HIGH);
+
+    config->pwm_mode = PWM_OFF;
+    config->gpio_num = gpio;
 
     return 0;
 }
